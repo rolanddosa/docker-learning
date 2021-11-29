e1dec8b0815f384d4385ca0bbaa3ced2bf093f58
rolanddosa <dosaroland@yahoo.com>
2021-11-29 17:21:27 +0000

    REST endpoint accessed via Docker locally!

--- src/main/java/com/example/dockerpractice/DockerPracticeApplication.java
+++ src/main/java/com/example/dockerpractice/DockerPracticeApplication.java
@@ -3,6 +3,9 @@ package com.example.dockerpractice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * https://www.youtube.com/watch?v=FlSup_eelYE
+ */
 @SpringBootApplication
 public class DockerPracticeApplication {
 
